8c1ed06e35c9b41372369dcbb4b2f92232994398
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-06-13 11:10:43 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())return false;
+        int arr[26] = {0};
+        for(char ch: s){
+            arr[ch - 'a']++;
+        }
+        for(char ch: t){
+            arr[ch - 'a']--;
+        }
+        for(int i=0; i < 26; i++){
+            if(arr[i] != 0)return false;
+        }
+        return true;
+    }
+};
