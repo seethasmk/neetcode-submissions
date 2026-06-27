adc2abca429ec2bbe7686072857555baab3b3402
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-06-26 18:04:10 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> nums;
+        while(n != 1){
+            if(nums.count(n))return false;
+            nums.insert(n);
+            n = sumOfSquares(n); 
+        }
+        return true;
+    }
+    int sumOfSquares(int x){
+        int sum = 0;
+        while(x > 0){
+            int digit = x % 10;
+            sum += (digit * digit);
+            x /= 10;
+        }
+        return sum;
+    }
+};
