859f03de23c15f93d8bc35291fca7eec0e790ad0
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-06-27 17:59:26 -0700

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> arr(n+1,0);
+        arr[1] = 1;
+        if(n >= 2)arr[2] = 2;
+        for(int i=3; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        return arr[n];
+    }
+};
