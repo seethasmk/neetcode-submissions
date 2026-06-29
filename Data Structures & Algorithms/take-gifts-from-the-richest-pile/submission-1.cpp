94159bf86f9ba310f8445e805a0c8bf323b27129
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-06-29 16:00:37 -0700

    Add: take-gifts-from-the-richest-pile - submission-1

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> pq(gifts.begin(), gifts.end());
+        long long s = 0;
+        while(k--){
+            int val = pq.top();
+            pq.pop();
+            val = floor(sqrt(val));
+            pq.push(val);
+        }
+        while(!pq.empty()){
+            s += pq.top();
+            pq.pop();
+        }
+        return s;
+    }
+};
