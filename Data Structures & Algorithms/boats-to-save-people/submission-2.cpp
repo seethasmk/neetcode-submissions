c11d2c51be486c520ab3ca1377af93e4ae2bd6ed
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-08-03 18:12:10 -0700

    Add: boats-to-save-people - submission-2

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end(), greater<>());
+        int l = 0; int r = people.size()-1;
+        int c = 0;
+        while(l <= r){
+            if((people[r] + people[l]) <= limit){
+                r--;
+            }
+            l++;
+            c++;
+        }
+        return c;
+    }
+};
