93b566ede454493c7635db85e77c8845fca2b99c
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-08-21 15:01:39 -0700

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> vals;
+        while(n != 1){
+            int sum = 0;
+            while(n > 0){
+                sum+= pow(n%10,2);
+                n/=10;
+            }
+            if(vals.count(sum))return false;
+            vals.insert(sum);
+            n= sum;
+        }
+        return true;
+    }
+};
