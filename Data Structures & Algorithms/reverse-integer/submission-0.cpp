f16993996dc81415db33d80f4ff43f412abe3bb5
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-08-28 01:29:03 -0700

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+        
+        while(x != 0){
+            int d = x % 10;
+            x /= 10;
+
+            if(rev > INT_MAX/10 || (rev == INT_MAX/10 && d > 7))return 0;
+            if(rev < INT_MIN/10 || (rev == INT_MIN/10 && d < -8))return 0;
+            rev = rev * 10 + d;
+        }
+        return rev;
+    }
+};
