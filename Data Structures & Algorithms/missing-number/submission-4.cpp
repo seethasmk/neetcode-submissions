b8d606bbc755a15ee41e83aa0d9f4c625d253d51
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-08-28 00:59:43 -0700

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n * (n+1)/2;
+        int val = 0;
+        for(int i=0; i < nums.size(); i++){
+            val += nums[i];
+        }
+        return sum - val;
+    }
+};
