e61dac8b9fa334724194d6c9a5497071a8bde1a1
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-09-02 15:20:59 -0700

    Add: contains-duplicate-ii - submission-1

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> wdw;
+        for(int i=0; i < nums.size(); i++){
+            if(wdw.count(nums[i]))return true;
+            wdw.insert(nums[i]);
+            if(wdw.size() > k){
+                wdw.erase(nums[i-k]);
+            }
+        }
+        return false;
+    }
+};
