79a195b2242c6c8825b070d7828faa0e5b17ab64
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-09-05 19:11:59 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<array<int,26>, vector<string>> anagrams;
+        for(int i=0; i < strs.size(); i++){
+            array<int,26> count = {0};
+            for(char ch: strs[i]){
+                count[ch - 'a']++;
+            }
+            anagrams[count].push_back(strs[i]);
+        }
+        vector<vector<string>> res;
+        for(auto& [key, val]: anagrams){
+            res.push_back(val);
+        }
+        return res;
+    }
+};
