9e366629d9fcca844f3024a01d855d928c37e2d4
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-09-05 17:32:13 -0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> vals;
+        for(int i=0; i < nums.size(); i++){
+            int val = target - nums[i];
+            if(vals.count(val))return{vals[val], i};
+            vals[nums[i]] = i;
+        }
+        return {};
+    }
+};
