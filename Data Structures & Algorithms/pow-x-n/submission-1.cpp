aaeaec4636a0126befca55625e205f022842b06d
Seethalakshmi Selvamuthu Kumarasami <seethalakshmis0307@gmail.com>
2026-09-10 10:49:45 -0700

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long N = n;
+        if(N < 0){
+            x = 1/x;
+            N = -N;
+        }
+        return pf(x,N);
+    }
+    double pf(double x, long n){
+        if(n == 0)return 1.0;
+        double half = pf(x, n/2);
+        if(n % 2 == 0){
+            return half * half;
+        }
+        else{
+            return half * half * x;
+        }
+    }
+};
